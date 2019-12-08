97610db342f2a8e579647ac7739b261a91b6f295
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2019-12-08 20:25:22 +0700

    init commit

---
+++ pages/auth/login/callback/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h4>Hi {{ user.name }}</h4>
+    <p>Youre logged by Github</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {}
+    };
+  },
+  mounted() {
+    let code = this.$route.query.code;
+    this.$axios
+      .$get(`${process.env.BASE_API_URL}/auth/login/callback?code=${code}`)
+      .then(res => (this.user = res.user))
+      .catch(err => console.log(err));
+  }
+};
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 offset-md-3 card p-5 mt-5">
+        <h3>Hi, Plase Login First</h3>
+        <button class="btn btn-primary mt-3" @click="login">Login</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    login() {
+        window.location.href = `${process.env.BASE_API_URL}/auth/login`
+    }
+  }
+};
+</script>
+
+<style></style>
